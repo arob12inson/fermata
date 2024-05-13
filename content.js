63c3631d5accc0f8e829169cda7d09dc35645d27
Aidan Robinson <120530116+arob26inson@users.noreply.github.com>
2024-05-12 23:19:29 -0700

    refactor pausing

--- content.js
+++ content.js
@@ -11,15 +11,3 @@ document.addEventListener("keydown", function (event) {
     }
   }
 });
-
-// Check if video element is available every 1000 milliseconds
-const intervalId = setInterval(() => {
-  const video = document.querySelector("video");
-  if (video) {
-    console.log("Video element found, setting up key listener.");
-    setupKeyListener();
-    clearInterval(intervalId); // Stop checking once the video is found
-  } else {
-    console.log("Video element not found, retrying...");
-  }
-}, 1000);
